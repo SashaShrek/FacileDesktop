99f7c87f1a746d79b332d251c089fff1bdea383d
SsSha256 <sssha256@yahoo.com>
2020-10-29 18:12:12 +0300

    Changed install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+wget https://github.com/SsSha256/FacileDesktop/releases/download/v.0.1/FDtar.tar.gz2
+tar -xvf FDtar.tar.gz2
+cd FDTar
 make
 sudo make install
 mkdir "$HOME/Conf"
-echo Sdsdsd
+cd ..
+rm -r FDTar FDtar.tar.gz2
